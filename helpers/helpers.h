0df5ec1980ca2c687359d6ac7da4f1f7efd8a4e2
Tommy Sadiq Hinrichsen <th@mustache.dk>
2018-03-01 08:51:36 +0100

    Initial Commit

---
+++ helpers/helpers.h
@@ -0,0 +1,19 @@
+//
+//  helpers.h
+//  helpers
+//
+//  Created by Tommy Hinrichsen on 01/03/2018.
+//  Copyright © 2018 EazyIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for helpers.
+FOUNDATION_EXPORT double helpersVersionNumber;
+
+//! Project version string for helpers.
+FOUNDATION_EXPORT const unsigned char helpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <helpers/PublicHeader.h>
+
+
